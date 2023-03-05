6eab257af7b583b26ebec67b38abd94a397cc573
jcruz265 <hello@johncruz.me>
2023-03-04 19:40:32 -0800

    html_files

---
+++ Pokémon - National Pokédex Pokémon_files/verify.js
@@ -0,0 +1,10 @@
+
+
+try{__tagObject_callback_127577231841({ImpressionID:"9025f976f9644abeb631ab26cfb8b3a3", ServerPublicDns:"rtbc-uw2.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "9025f976f9644abeb631ab26cfb8b3a3");}catch(e){}
+try{__verify_callback_127577231841({
+ResultID:2,
+Passback:"",
+AdWidth:728,
+AdHeight:90});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "9025f976f9644abeb631ab26cfb8b3a3");}catch(e){}
